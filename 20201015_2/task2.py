cb0c767c324c8dcc5d7b66210ce81e53607251ef
Алексей Казантаев <a.kazantaev@gmail.com>
2020-10-20 09:26:18 +0300

    task2 complete

--- 20201015_2/task2.py
+++ 20201015_2/task2.py
@@ -1,8 +1,30 @@
-import random
-import sys
+# Генерация произносимого пароля
+#
+# произносимый пароль состоит из "слогов" трёх видов: 'Г', 'ГС' 'СГ',
+#       где 'Г' - заглавная гласная буква, 'С' - заглавная согласная буква (Ь и Ъ не используем)
+#
+# на вход подаётся длина пароля (целое положительное число N)
+# требуется сгенерировать пароль длины N путём его достройки, начиная с пустой строки,
+#       до требуемой длины путём добавления случайного слога одного из описанных выше видов
+# к частично построенной строке длины (N-1) разрешается добавлять последний слог из двух букв
 
-glas = 'аеёиоуыэюя'.upper()
-soglas = 'бвгджзклмнпрстфхцчшщ'.upper()
-passw_len = sys.argv[1]
+
+from random import *
+from sys import argv
+
+glas = list('АЕЁИОУЫЭЮЯ')
+soglas_space = soglas = list('БВГДЖЗКЛМНПРСТФХЦЧШЩ')
+soglas_space.append('')
+all_letters = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЭЮЯ'
+passw_len = int(argv[1])
 passw = ''
-
+len = 0
+while len < passw_len:
+    rand_letter1 = choice(all_letters)
+    if rand_letter1 in glas:
+        rand_letter2 = choice(soglas_space)
+    else:
+        rand_letter2 = choice(glas)
+    passw += rand_letter1 + rand_letter2
+    len += 1 + (rand_letter2 != '')
+print(passw)
