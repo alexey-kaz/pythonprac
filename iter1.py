0b88f1333bbdc3a15f8f6540a035360d0da02b2e
Алексей Казантаев <a.kazantaev@gmail.com>
2020-10-25 14:09:42 +0300

    del
    Former-commit-id: 292af0922bb115c95b6ce15ab05a9ab007c827b6

--- iter1.py
+++
@@ -1,9 +0,0 @@
-def ifun():
-    i = 1
-    while i:
-        yield i**2
-        i+=1
-
-I = ifun()
-
-print(*I)
--- iter2.py
+++
@@ -1,9 +0,0 @@
-def fib(m,n):
-    a = 1
-    b = 1
-    for i in range(n):
-        if i+1 >= m:
-            yield a
-        a, b = b, a+b
-
-print(*fib(3,5))
