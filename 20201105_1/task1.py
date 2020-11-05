93dcc0b24af3204aff578f20978b56d5f14e3c2e
Казантаев Алексей <s02180645@live.cs.msu.ru>
2020-11-05 17:54:38 +0300

    task1 fix
    Former-commit-id: d6dbd2c8e31fa6cdfdb8dd2e0524b4b1e23fe871

--- 20201105_1/task1.py
+++ 20201105_1/task1.py
@@ -1,6 +1,6 @@
 from re import *
 def match_check(st):
-    return bool(match(r"[+-]?\d*.?\d+[Ee]*[+-]*\d*", st))
+    return bool(match(r"[+-]?\d*.?\d+[Ee]*[+-]*\d*[^\w]", st))
 
 while s := input():
     print(match_check(s))
