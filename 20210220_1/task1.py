a2fd543118a6ad439b8d9fa4922efe83c20687c0
alexey-kaz <a.kazantaev@hotmail.com>
2021-02-26 04:24:16 +0300

    Buttons and Label added

--- 20210220_1/task1.py
+++ 20210220_1/task1.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -7,9 +8,14 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
+        self.timeButton = tk.Button(self, text='Time')
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.timeLabel = tk.Label(self)
+        self.timeButton.grid()
         self.quitButton.grid()
+        self.timeLabel.grid()
+
 
 app = Application()
-app.master.title('Sample application')
+app.master.title('Sample timer application')
 app.mainloop()
