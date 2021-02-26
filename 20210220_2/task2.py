2d6c8c1ac0a41cf0df553fc73691834abbb04a2c
alexey-kaz <a.kazantaev@hotmail.com>
2021-02-26 15:57:24 +0300

    git button

--- 20210220_2/task2.py
+++ 20210220_2/task2.py
@@ -13,13 +13,18 @@ class Application(tk.Frame):
     def showdate(self):
         self.bttn.set(run("date", capture_output=True, shell=True).stdout)
 
+    def showgit(self):
+        self.bttn.set(run("git", capture_output=True, shell=True).stdout)
+
     def createWidgets(self):
         self.bttn = tk.StringVar()
         self.dirButton = tk.Button(self, text='dir', command=self.showdir)
         self.dateButton = tk.Button(self, text='date', command=self.showdate)
+        self.gitButton = tk.Button(self, text='git', command=self.showgit)
         self.textLabel = tk.Label(self, textvariable=self.bttn)
         self.dirButton.grid(row=0, column=1)
         self.dateButton.grid(row=0, column=2)
+        self.gitButton.grid(row=0, column=3)
         self.textLabel.grid(columnspan=4)
 
 
