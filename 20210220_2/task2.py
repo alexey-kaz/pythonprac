9fb1b2f4f5b27ec5317f2d8001178dab12cd63ba
alexey-kaz <a.kazantaev@hotmail.com>
2021-02-26 15:51:10 +0300

    dir button

---
+++ 20210220_2/task2.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from subprocess import run
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def showdir(self):
+        self.bttn.set(run("dir", capture_output=True, shell=True).stdout)
+
+    def createWidgets(self):
+        self.bttn = tk.StringVar()
+        self.dirButton = tk.Button(self, text='dir', command=self.showdir)
+        self.textLabel = tk.Label(self, textvariable=self.bttn)
+        self.dirButton.grid(row=0, column=1)
+        self.textLabel.grid(columnspan=4)
+
+
+app = Application()
+app.master.title('task2.py')
+app.mainloop()
