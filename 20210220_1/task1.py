e92950391da45bf477cb2e11088c96d6b3990cae
alexey-kaz <a.kazantaev@hotmail.com>
2021-02-26 04:28:34 +0300

    StringVar

--- 20210220_1/task1.py
+++ 20210220_1/task1.py
@@ -7,14 +7,16 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
+        self.showtime()
 
     def showtime(self):
-        self.timeLabel['text'] = time.ctime(time.time())
+        self.time.set(time.strftime("%c"))
 
     def createWidgets(self):
+        self.time = tk.StringVar()
         self.timeButton = tk.Button(self, text='Time', command=self.showtime)
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.timeLabel = tk.Label(self)
+        self.timeLabel = tk.Label(self, textvariable=self.time)
         self.timeButton.grid()
         self.quitButton.grid(row=0, column=1)
         self.timeLabel.grid(columnspan=2)
