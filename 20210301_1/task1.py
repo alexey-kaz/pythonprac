d0fad27e603c34a04861cf25ae54522128220f6f
Казантаев Алексей <s02180645@live.cs.msu.ru>
2021-03-01 12:11:44 +0300

    Exit button

--- 20210301_1/task1.py
+++ 20210301_1/task1.py
@@ -3,6 +3,16 @@ import tkinter as tk
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+    def createWidgets(self):
+        """
+        Buttons:
+        -------
+        exit - closes window
+        """
+        self.exitButton = tk.Button(self, text='Exit')
+        self.exitButton.grid()
 
 
 app = Application()
