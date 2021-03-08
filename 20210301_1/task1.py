bae5d56f49dc9183895274c65185bb829e2f3d73
alexey-kaz <a.kazantaev@hotmail.com>
2021-03-08 18:36:38 +0300

    1-3 Item text

--- 20210301_1/task1.py
+++ 20210301_1/task1.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
+
     def createWidgets(self):
         """
         Buttons:
@@ -12,7 +14,14 @@ class Application(tk.Frame):
         exit - closes window
         """
         self.exitButton = tk.Button(self, text='Exit')
-        self.exitButton.grid()
+        self.exitButton.grid(row=0, column=1)
+        """ 
+        Labels: 
+        _______
+        <MenuItem>
+        """
+        self.textLabel = tk.Label(self, text='<MenuItem>')
+        self.textLabel.grid(row=1, column=1)
 
 
 app = Application()
